28ef7c514867f2a495a9c78ae7468f6fe96ebda2
Jess <jessantillo@gmail.com>
2020-09-08 16:06:37 -0300

    Fix-bug Criar departamentos

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -2,7 +2,7 @@ class Produto < ApplicationRecord
 
     belongs_to :departamento, optional: true
 
-    validates :preco, presence: true
+    validates :preco, :departamento, presence: true
     validates :nome, length: {minimum: 4}
 
 end
